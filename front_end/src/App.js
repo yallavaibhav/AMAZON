27620a38c79ec469a52b0e0a8f3215f144240179
yallavaibhav <yallavaibhav@gmail.com>
2022-06-09 14:38:57 -0700

    Create-React-APP

---
+++ front_end/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;

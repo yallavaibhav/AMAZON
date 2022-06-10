18f739a2f4b64624b935be405c4637403cd905a9
yallavaibhav <yallavaibhav@gmail.com>
2022-06-09 18:58:45 -0700

    added images and formating

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -7,7 +7,27 @@ function App() {
       <header>
         <a href="/">amazon</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1> Featured products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={'/product/${product.slug}'}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="Productinfo">
+                <a href={'/product/${product.slug}'}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
